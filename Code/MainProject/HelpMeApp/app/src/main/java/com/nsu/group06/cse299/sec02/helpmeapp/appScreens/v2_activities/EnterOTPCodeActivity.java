91d0da2aa23d41d92f51455840e4eb8152f4480b
ferdouszislam <ferdouszislam@gmail.com>
2021-04-19 22:34:01 +0600

    Enhancement, UI #1: Enter OTP code activity layout created.

---
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/appScreens/v2_activities/EnterOTPCodeActivity.java
@@ -0,0 +1,16 @@
+package com.nsu.group06.cse299.sec02.helpmeapp.appScreens.v2_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nsu.group06.cse299.sec02.helpmeapp.R;
+
+public class EnterOTPCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_otp_code);
+    }
+}
