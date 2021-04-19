1241442caba7588ff14187aa625b4d0c303cf4d3
ferdouszislam <ferdouszislam@gmail.com>
2021-04-19 22:35:11 +0600

    Enhancement, UI #1: activity_enter_phone_number.xml update.

--- Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/appScreens/v2_activities/EnterPhoneNumberActivity.java
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/appScreens/v2_activities/EnterPhoneNumberActivity.java
@@ -2,7 +2,9 @@ package com.nsu.group06.cse299.sec02.helpmeapp.appScreens.v2_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.nsu.group06.cse299.sec02.helpmeapp.R;
 
@@ -12,5 +14,10 @@ public class EnterPhoneNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_phone_number);
+    }
+
+    public void submitClick(View view) {
+
+        startActivity(new Intent(this, EnterOTPCodeActivity.class));
     }
 }
