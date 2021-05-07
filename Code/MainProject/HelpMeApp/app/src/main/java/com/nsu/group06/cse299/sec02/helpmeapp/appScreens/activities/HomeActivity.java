6f8fa71b5f8c129fbde7e50d7eaf1dc90b56007b
ferdouszislam <ferdouszislam@gmail.com>
2021-05-07 19:53:31 +0600

    Enhancement #4: HomeActivity update.
    Request location permission then background location permission and then location settings before starting EmergencyModeService

--- Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/appScreens/activities/HomeActivity.java
+++ Code/MainProject/HelpMeApp/app/src/main/java/com/nsu/group06/cse299/sec02/helpmeapp/appScreens/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.nsu.group06.cse299.sec02.helpmeapp.appScreens.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.work.Constraints;
@@ -8,6 +9,7 @@ import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,12 +17,20 @@ import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.nsu.group06.cse299.sec02.helpmeapp.R;
 import com.nsu.group06.cse299.sec02.helpmeapp.appScreens.v2_activities.FindSafePlacesActivity;
 import com.nsu.group06.cse299.sec02.helpmeapp.appScreens.v2_activities.InternetAlertActivity;
 import com.nsu.group06.cse299.sec02.helpmeapp.appScreens.v2_activities.MenuActivity;
 import com.nsu.group06.cse299.sec02.helpmeapp.background.services.EmergencyModeService;
 import com.nsu.group06.cse299.sec02.helpmeapp.background.workers.NotifyNearbyHelpPostWorker;
+import com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation.LocationFetcher;
+import com.nsu.group06.cse299.sec02.helpmeapp.fetchLocation.fusedLocationApi.FusedLocationFetcherApiAdapter;
 import com.nsu.group06.cse299.sec02.helpmeapp.sharedPreferences.AppSettingsSharedPref;
 
 import java.util.concurrent.TimeUnit;
@@ -44,6 +54,26 @@ public class HomeActivity extends InternetAlertActivity {
     // emergency mode state shared preference
     private AppSettingsSharedPref mAppSettingsSharedPref;
 
+    // location setup variables for EmergencyMode
+    private LocationFetcher mLocationFetcher;
+    private LocationFetcher.LocationSettingsSetupListener mLocationSettingsSetupListener =
+            new LocationFetcher.LocationSettingsSetupListener() {
+                @Override
+                public void onLocationSettingsSetupSuccess() {
+
+                    startEmergencyModeService();
+                    Log.d(TAG, "onLocationSettingsSetupSuccess: location settings met.");
+                }
+
+                @Override
+                public void onLocationSettingsSetupFailed(String message) {
+
+                    // user will be automatically be asked to enable location settings
+                    // see method in HelpPostActivity: 'onActivityResult(...)'
+                    Log.d(TAG, "onLocationSettingsSetupFailed: location settings setup failed ->" + message);
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +82,33 @@ public class HomeActivity extends InternetAlertActivity {
         init();
 
         startNearbyHelpPostsNotificationWorker();
+    }
+
+    /*
+    Required for reacting to user response when setting up required location settings
+    user response to default "turn on location" dialog is handled through this method
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+
+            case LocationFetcher.REQUEST_CHECK_LOCATION_SETTINGS:
+
+                if(resultCode==RESULT_OK){
+                    // user enabled location settings
+                    startEmergencyModeService();
+                }
+
+                else{
+                    // location settings not met
+                    showLocationSettingsExplanationDialog();
+                    mEmergencyModeSwitch.setChecked(false);
+                    mAppSettingsSharedPref.setEmergencyModeState(false);
+                }
+                break;
+        }
     }
 
     private void init() {
@@ -64,6 +121,11 @@ public class HomeActivity extends InternetAlertActivity {
 
         mAppSettingsSharedPref = AppSettingsSharedPref.build(this);
         mEmergencyModeSwitch.setChecked(mAppSettingsSharedPref.getEmergencyModeState(false));
+
+        mLocationFetcher =
+                new FusedLocationFetcherApiAdapter(
+                        EmergencyModeService.EMERGENCY_MODE_LOCATION_UPDATE_INTERVAL,
+                        this, mLocationSettingsSetupListener);
     }
 
     private void startNearbyHelpPostsNotificationWorker() {
@@ -91,11 +153,84 @@ public class HomeActivity extends InternetAlertActivity {
 
         mAppSettingsSharedPref.setEmergencyModeState(mEmergencyModeSwitch.isChecked());
 
-        if(mEmergencyModeSwitch.isChecked()) startEmergencyModeService();
+        if(mEmergencyModeSwitch.isChecked()) getBackgroundLocationPermission();
         else stopEmergencyModeService();
     }
 
+    /**
+     * Ask for location access permission
+     * and then start EmergencyModeService or ask for background location
+     */
+    private void getLocationPermission() {
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                        getBackgroundLocationPermission();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                        showLocationPermissionExplanationDialog(permissionDeniedResponse.isPermanentlyDenied());
+                        mEmergencyModeSwitch.setChecked(false);
+                        mAppSettingsSharedPref.setEmergencyModeState(false);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        // ignore for now
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+
+    /**
+     * Ask for background location permission
+     * and then start EmergencyModeService
+     */
+    private void getBackgroundLocationPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Dexter.withContext(this)
+                    .withPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    .withListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                            mLocationFetcher.setupLocationSettings(HomeActivity.this);
+                        }
+
+                        @Override
+                        public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            showLocationPermissionExplanationDialog(permissionDeniedResponse.isPermanentlyDenied());
+                            mEmergencyModeSwitch.setChecked(false);
+                            mAppSettingsSharedPref.setEmergencyModeState(false);
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                            // ignore for now
+                            permissionToken.continuePermissionRequest();
+                        }
+                    })
+                    .check();
+        }
+
+        else mLocationFetcher.setupLocationSettings(this);
+    }
+
     private void startEmergencyModeService() {
+
+        // instant bug fix
+        // in case permission is denied but after showing explanation dialog permission is granted
+        // then before starting the service mEmergencyModeSwitch needs to be switched on
+        mEmergencyModeSwitch.setChecked(true);
 
         Intent intent = new Intent(this, EmergencyModeService.class);
 
@@ -184,5 +319,54 @@ public class HomeActivity extends InternetAlertActivity {
 
         mSnackbar.show();
         mIsInternetAvailable = false;
+    }
+
+    /*
+    show alert dialog explaining why location permission is a MUST
+    with a simple dialog, quit activity if permission is permanently denied
+    courtesy - <https://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+     */
+    private void showLocationPermissionExplanationDialog(boolean isPermissionPermanentlyDenied) {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        String title = getString(R.string.location_permission);
+        String explanation;
+
+        if(isPermissionPermanentlyDenied)
+            explanation = getString(R.string.location_permission_permanantely_denied_explanation);
+        else
+            explanation = getString(R.string.location_permission_explanation);
+
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(explanation);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
+                (dialog, which) -> {
+                    if(!isPermissionPermanentlyDenied)
+                        getLocationPermission();
+                    else
+                        finish();
+                });
+
+        alertDialog.show();
+    }
+
+    /*
+    show alert dialog explaining why location settings MUST be enabled
+    courtesy - <https://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+     */
+    private void showLocationSettingsExplanationDialog() {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        String title = getString(R.string.location_settings);
+        String explanation = getString(R.string.location_settings_explanation);
+
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(explanation);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
+                (dialog, which) -> mLocationFetcher.setupLocationSettings(this));
+
+        alertDialog.show();
     }
 }
